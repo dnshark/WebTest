1fe26b81fe3c27db9752ee949a50ca22649e1a31
dnshark <dnshark@mail.ru>
2015-12-19 12:26:34 +0200

    rename

--- main/java/nedis/study/jee/utils/Calculation.java
+++ main/java/nedis/study/jee/utils/Calculation.java
@@ -4,8 +4,8 @@ package nedis.study.jee.utils;
  * Created by Дмитрий on 19.12.2015.
  */
 public class Calculation {
-    public static int getMaxPage(Long page,Integer count){
-        Double d = (double) page;
+    public static int getMaxPage(Long allCount,Integer count){
+        Double d = (double) allCount;
         return (int)Math.ceil(d / count);
     }
 }
